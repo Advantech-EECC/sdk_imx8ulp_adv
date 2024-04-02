d1206c8c8135bd68a0ffcbcf21e03c4642f06e7e
F. Aragon <francisco.aragon@advantech.de>
2024-04-02 16:03:26 +0200

    fix: build scripts exit code propagation

--- application/armgcc/build_debug.sh
+++ application/armgcc/build_debug.sh
@@ -5,3 +5,4 @@ if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
 cmake -DCMAKE_TOOLCHAIN_FILE="../../sdk/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug  .
 make -j 2>&1 | tee build_log.txt
+exit ${PIPESTATUS[0]}
--- application/armgcc/build_flash_debug.sh
+++ application/armgcc/build_flash_debug.sh
@@ -5,3 +5,4 @@ if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
 cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flash_debug  .
 make -j 2>&1 | tee build_log.txt
+exit ${PIPESTATUS[0]}
