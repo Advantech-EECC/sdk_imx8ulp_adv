Advantech-EECC/sdk_imx8ulp_adv

commit caa85f48b06567de5120c91767a2ba0f5d301264

UART TX optimization
Iterate after writing to the FIFO, so if after one write, more are
possible afterwards. Rationale: > 1Mbps clocks could provide some
benefit. With the tests at 1Mbps (~100KB/s), no significant transfer
increase have been observed.

application/main_remote.c
@@ -286,13 +286,16 @@ void app_subtask_uart_tx(RxTxContext *c)
 
     if (c->size > 0)
     {
+        uint32_t tx;
         uint8_t *buf = c->buf + c->ndx;
-        uint32_t tx = write_lpuart(&lpuart2, buf, c->size);
 
-        c->ndx += tx;
-        c->size -= tx;
+        do {
+            tx = write_lpuart(&lpuart2, buf, c->size);
+            c->ndx += tx;
+            c->size -= tx;
+        } while (tx > 0);
 
-        if (c->size == 0)
+        if (c->size == 0) // prepare ndx for next transfer
             c->ndx = 0;
 
         PRINTF_UART("LPUART TX %u left %u\r\n", tx, c->size);

commit 390efb864960bd7d6169e9a575357b240e8589db

fix: UART TX subtax fixes
Fix provided by Electrolux. The issue was introduced in commit caa85f4
when adding a late optimization. In addition to the speed tests, we'll
add always the integrity tests before a release for avoiding this
happening again.

caa85f48b06567de5120c91767a2ba0f5d301264

application/main_remote.c
@@ -278,16 +278,19 @@ void app_subtask_uart_tx(RxTxContext *c)
     if (c->size > 0)
     {
         uint32_t tx;
-        uint8_t *buf = c->buf + c->ndx;
 
         do {
-            tx = write_lpuart(&lpuart2, buf, c->size);
+            uint8_t *buf = c->buf + c->ndx;
+            tx = write_lpuart(&lpuart2, buf, c->size - c->ndx);
             c->ndx += tx;
-            c->size -= tx;
-        } while (tx > 0 && c->size > 0);
+        } while (tx > 0 && c->ndx != c->size);
 
-        if (c->size == 0) // prepare ndx for next transfer
+        if (c->ndx == c->size)
+        {
+            // prepare ndx for next transfer
             c->ndx = 0;
+            c->size = 0;
+        }
 
         PRINTF_UART("LPUART TX %u left %u\r\n", tx, c->size);
     }